aab54667619af66aed53d04a39b4ef5996f71c97
Moh Yusuf <muhamadyusuf03112001@gmail.com>
2023-11-27 19:56:05 +0700

    first

--- Js/table.js
+++ Js/table.js
@@ -14,6 +14,12 @@ document.addEventListener("DOMContentLoaded", function () {
       // Create a new row for the form data
       const newRow = document.createElement("tr");
 
+      // Create a cell for the 'No' column (row number)
+      const noCell = document.createElement("th");
+      noCell.setAttribute("scope", "row");
+      noCell.textContent = tableBody.childElementCount + 1; // Number rows sequentially
+      newRow.appendChild(noCell);
+
       // Create a cell for each field
       const fields = [
         "nama",
@@ -33,11 +39,13 @@ document.addEventListener("DOMContentLoaded", function () {
         newRow.appendChild(cell);
       });
 
-      // Create a cell for the 'No' column (row number)
-      const noCell = document.createElement("th");
-      noCell.setAttribute("scope", "row");
-      noCell.textContent = tableBody.childElementCount + 1; // Number rows sequentially
-      newRow.insertBefore(noCell, newRow.firstChild); // Insert as first cell
+      // Create a cell for the delete button
+      const deleteCell = document.createElement("td");
+      const deleteButton = document.createElement("button");
+      deleteButton.textContent = "Delete";
+      deleteButton.classList.add("delete-btn");
+      deleteCell.appendChild(deleteButton);
+      newRow.appendChild(deleteCell);
 
       // Append the new row to the table body
       tableBody.appendChild(newRow);
@@ -46,4 +54,20 @@ document.addEventListener("DOMContentLoaded", function () {
 
   // Call function to populate the table on page load
   populateTable();
+
+  // Add event listeners to dynamically added delete buttons
+  document.addEventListener("click", function (event) {
+    if (event.target.classList.contains("delete-btn")) {
+      const row = event.target.closest("tr");
+      row.remove(); // Remove the row from the table
+
+      // Here, you can also delete the corresponding data from localStorage based on the row's content
+      // For example, retrieve the ID or unique identifier associated with the row and delete it from localStorage
+    }
+  });
+});
+
+// jQuery code to add a class to a button
+$(document).ready(function () {
+  $(".delete-btn").addClass("btn btn-danger");
 });
